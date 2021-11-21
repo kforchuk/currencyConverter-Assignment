4f67c3d1bd0ade0d91051d31c664d4520a43bcd8
Katrina Forchuk <katrinaforchuk@gmail.com>
2021-11-20 21:55:38 -0700

    Uploaded Full Project

---
+++ src/app/services/exchange-rates.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ExchangeRatesResponse} from './payloads/exchange-rates-response';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyExchangeService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getRates(base: string): Observable<ExchangeRatesResponse> {
+    return this.http.get<ExchangeRatesResponse>(`https://api.exchangerate.host/latest?source=ecb&base=${base}`);
+  }
+}
